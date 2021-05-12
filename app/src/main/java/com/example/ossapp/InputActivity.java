0ce684a9a0149dd7175cf10a59603ea1f893471c
vlymar90 <7ua.lymar2014@gmail.com>
2021-05-12 14:44:53 +0300

    done 3 Stage

--- app/src/main/java/com/example/ossapp/InputActivity.java
+++ app/src/main/java/com/example/ossapp/InputActivity.java
@@ -3,30 +3,38 @@ package com.example.ossapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.SystemClock;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import com.example.ossapp.util.UtilButton;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class InputActivity extends AppCompatActivity {
       public static final String NUMBER = "message";
-      private String nameButton;
+      private String textButton = "Запросить повторно через 00:%d";
+      private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
-        Button button = (Button) findViewById(R.id.button);
+        button = (Button) findViewById(R.id.button);
         EditText editText = (EditText)findViewById(R.id.codeInput);
         Intent intent = getIntent();
         String number = intent.getStringExtra(NUMBER);
         TextView info = (TextView) findViewById(R.id.textViewInfo);
         info.setText("Код отправлен на номер "  + number);
-//        UtilButton.setButtonText(button);
+        button.setEnabled(false);
+        setTextButton();
+
         editText.setOnKeyListener(new View.OnKeyListener() {
             @Override
             public boolean onKey(View v, int keyCode, KeyEvent event) {
@@ -42,6 +50,22 @@ public class InputActivity extends AppCompatActivity {
     }
     public void repeatCode(View view) {
         //логика отправки запроса на сервер
-//        UtilButton.setButtonText(button);
+          button.setEnabled(false);
+          setTextButton();
+    }
+    private void setTextButton() {
+        new CountDownTimer(59000, 1000) {
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                button.setText(String.format(textButton, millisUntilFinished/1000));
+            }
+
+            @Override
+            public void onFinish() {
+                button.setText("повторить");
+                button.setEnabled(true);
+            }
+        }.start();
     }
 }
