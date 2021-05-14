df71d6756f1a0659ae434750108229185e2c1471
vlymar90 <7ua.lymar2014@gmail.com>
2021-05-14 14:46:57 +0300

    done Stage6

--- app/src/main/java/com/example/ossapp/FightingStylesActivity.java
+++ app/src/main/java/com/example/ossapp/FightingStylesActivity.java
@@ -1,14 +1,40 @@
 package com.example.ossapp;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import com.example.ossapp.User.User;
+import com.example.ossapp.util.UtilSelectStyle;
 
 public class FightingStylesActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fighting_styles);
+    }
+
+    public void selectStyle(View view) {
+        Button button = (Button) view;
+        if (User.getInstance().getUserStyleList().size() != 3) {
+            if (UtilSelectStyle.selectStyle(button.getText().toString())) {
+                button.setTextColor(Color.WHITE);
+                button.setBackgroundColor(Color.DKGRAY);
+            } else {
+                button.setTextColor(Color.BLACK);
+                button.setBackgroundColor(Color.LTGRAY);
+            }
+        }
+    }
+
+
+    public void nextStage(View view) {
+        if (User.getInstance().getUserStyleList().size() != 0) {
+            Intent intent = new Intent(this, null);
+            startActivity(intent);
+        }
     }
 }
