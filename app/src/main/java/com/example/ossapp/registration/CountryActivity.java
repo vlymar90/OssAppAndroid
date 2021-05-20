c08d4ffc0c8c95c3e050fc8bee1ef7b127252970
vlymar90 <7ua.lymar2014@gmail.com>
2021-05-20 15:56:29 +0300

    add countryactivity

---
+++ app/src/main/java/com/example/ossapp/registration/CountryActivity.java
@@ -0,0 +1,16 @@
+package com.example.ossapp.registration;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ossapp.R;
+
+public class CountryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_country);
+    }
+}
